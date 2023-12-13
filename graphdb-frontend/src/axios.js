290571339a3947e9c5a7f881138385940144f3a9
SnoxE <oicmyzs2000@gmail.com>
2023-12-13 16:07:49 +0100

    change port

--- graphdb-frontend/src/axios.js
+++ graphdb-frontend/src/axios.js
@@ -1,3 +1,4 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://graphdb-backend.onrender.com/'
+// axios.defaults.baseURL = 'https://graphdb-backend.onrender.com/'
+axios.defaults.baseURL = 'http://localhost:8080/'
